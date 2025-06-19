998e744a938b4e891b75c204d1180730d9355174
Lucas Gabriel <lucas.abade2104@gmail.com>
2025-06-19 20:12:29 -0300

    Update DAOColaborador.java
    Elaboração do DAOColaborador

--- LocaTest/DataAcessObject/DAOColaborador.java
+++ LocaTest/DataAcessObject/DAOColaborador.java
@@ -1,5 +1,40 @@
 package DataAcessObject;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Conexao.ConnectorJC;
+import Entidades.Colaborador;
 
 public class DAOColaborador {
-    
+
+    public void cadastrarColaborador(Colaborador colaborador) {
+
+        String sql = "INSERT INTO Colaborador (id_pessoa, matricula, senha) VALUES (?, ?, ?)";
+
+        PreparedStatement ps = null;
+
+        try {
+            ps = ConnectorJC.getConexao().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1, colaborador.getIdPessoa());
+            ps.setString(2, colaborador.getMatricula());
+            ps.setString(3, colaborador.getSenha());
+
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                int idGerado = rs.getInt(1);
+                colaborador.setIdColaborador(idGerado);
+            }
+
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+    } 
 }
