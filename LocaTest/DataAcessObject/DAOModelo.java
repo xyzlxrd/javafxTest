d0fec350aeca20f92d1d97977f4224a8ae21cd59
Lucas Gabriel <lucas.abade2104@gmail.com>
2025-06-19 21:22:53 -0300

    Update DAOModelo.java
    Elaborando DAOModelo

--- LocaTest/DataAcessObject/DAOModelo.java
+++ LocaTest/DataAcessObject/DAOModelo.java
@@ -1,19 +1,39 @@
 package DataAcessObject;
-import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
 import Conexao.ConnectorJC;
 import Entidades.Modelo;
 
 public class DAOModelo {
-    
-    public void cadastrarModelo (Modelo modelo) {
 
-        String sql = "INSERT INTO Modelo(nome_modelo, fabricante, ano_fabricacao) VALUES (?, ?, ?)";
+    public void cadastrarColaborador(Modelo modelo) {
+
+        String sql = "INSERT INTO Modelo (nome_modelo, fabricante, ano_fabricacao) VALUES (?, ?, ?)";
 
         PreparedStatement ps = null;
 
-    }
+        try {
+            ps = ConnectorJC.getConexao().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
+            ps.setInt(1, modelo.getIdModelo());
+            ps.setString(2, modelo.getFabricante());
+            ps.setInt(3, modelo.getAnoFabricacao().getValue());
+
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                int idGerado = rs.getInt(1);
+                modelo.setIdModelo(idGerado);
+            }
+
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+    } 
 }
