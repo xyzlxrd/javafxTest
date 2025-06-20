a9fef28e6b123b4029f63cf8bca7767216f8ad14
Lucas Gabriel <lucas.abade2104@gmail.com>
2025-06-20 18:45:44 -0300

    Update DAOCliente.java
    * Método "efetuarCadastro" adicionado. Permitirá com que o usuário efetue o login com base em seu nome de usuário e senha.
    
    * "Static" adicionado aos métodos para que seja possível de utiliza-los sem instânciar a classe DAOCliente.

--- LocaTest/DataAcessObject/DAOCliente.java
+++ LocaTest/DataAcessObject/DAOCliente.java
@@ -9,7 +9,7 @@ import Entidades.Cliente;
 
 public class DAOCliente {
 
-    public void cadastrarCliente(Cliente cliente) {
+    public static void cadastrarCliente(Cliente cliente) {
 
         String sql = "INSERT INTO Cliente (id_pessoa, usuario, senha) VALUES (?, ?, ?)";
 
@@ -34,7 +34,37 @@ public class DAOCliente {
 
         } catch (SQLException e) {
             e.printStackTrace();
+        }   
+    }
+
+    public static Cliente efetuarLogin(String usuario, String senha) {
+
+        Cliente clienteLogado = null;
+
+        String sql = "SELECT * FROM cliente WHERE usuario = ? AND senha = ?";
+
+        PreparedStatement ps = null;
+
+        try {
+            ps = ConnectorJC.getConexao().prepareStatement(sql);
+
+            ps.setString(1, usuario);
+            ps.setString(2, senha);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                clienteLogado = new Cliente();
+
+                clienteLogado.setIdCliente(rs.getInt("id_cliente"));
+                clienteLogado.setIdPessoa(rs.getInt("id_pessoa"));
+                clienteLogado.setUsuario(rs.getString("usuario"));
+                clienteLogado.setSenha(rs.getString("senha"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro na verificação do login/Cliente: "+e.getMessage());
         }
-        
-    } 
-}
+            return clienteLogado;
+        }
+    }
