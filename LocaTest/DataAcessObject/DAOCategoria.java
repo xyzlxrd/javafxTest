f5b0cf52f8e4747a81cdaf30a2d7e04ab55cd9c4
Lucas Gabriel <lucas.abade2104@gmail.com>
2025-06-19 21:29:17 -0300

    Update DAOCategoria.java

--- LocaTest/DataAcessObject/DAOCategoria.java
+++ LocaTest/DataAcessObject/DAOCategoria.java
@@ -4,19 +4,16 @@ import java.sql.SQLException;
 import Conexao.ConnectorJC;
 import Entidades.Categoria;
 
-public class DAOCategoria {//Data Acess Object, uma classe que possui métodos que fazem a conexão com o banco de dados
+public class DAOCategoria {
    
-    public void cadastrarCategoria (Categoria categoria) {//Método de Inserção de Dados
+    public void cadastrarCategoria (Categoria categoria) {//TESTAR
 
-        //Comando SQL que insere dados na respectiva tabela no Banco, no caso abaixo, a tabela "Categoria"
-        //As interrogações (?) entre parenteses após o "VALUES representam os valores que serão enviados por parâmetros
 
         String sql = "INSERT INTO Categoria(nome_categoria, valor_diaria) VALUES (?, ?)";
 
-        //Permite pré-compilar uma instrução SQL e executá-la várias vezes com diferentes parâmetros
         PreparedStatement ps = null;
 
-        try {//Chama a Classe e o Método de Conexão responsável pela conexão, bem como o uso do método ps(sql) para efetuar a inserção dos dados
+        try {
             ps = ConnectorJC.getConexao().prepareStatement(sql);
 
             ps.setString(1, categoria.getNomeCategoria());
@@ -28,7 +25,5 @@ public class DAOCategoria {//Data Acess Object, uma classe que possui métodos q
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-}
-
+    }
 }
